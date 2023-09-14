18b019ce93558303f18786a3c5d3b56e46a8cc88
oliverbaileysmith <smitholiverbailey@gmail.com>
2023-09-13 22:15:15 -0400

    Connect to MongoDB database

---
+++ backend/config.js
@@ -0,0 +1,3 @@
+export const PORT = 5555;
+
+export const mongoDBURL = "mongodb+srv://oliver:5vFMCXGBmPg3Z6XX4rFuLsFCisbn5Wsr@leaderboard.zdyzuv6.mongodb.net/scores?retryWrites=true&w=majority";
--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,24 @@
 import express from "express";
+import mongoose from "mongoose";
 
-const PORT = 5555;
+import { PORT, mongoDBURL } from "./config.js";
 
 const app = express();
 
-app.listen(PORT, () => {
-	console.log(`App is listening to port: ${PORT}`);
+app.get("/", (req, res) => {
+	console.log(req);
+	return res.status(234).send("Welcome to the leaderboard!");
 });
+
+mongoose
+	.connect(mongoDBURL)
+	.then(() => {
+		console.log("App connected to MongoDB.");
+
+		app.listen(PORT, () => {
+			console.log(`App is listening to port: ${PORT}`);
+		});
+	})
+	.catch((error) => {
+		console.log(error);
+	});
