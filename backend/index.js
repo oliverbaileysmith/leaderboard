228ed96dbebab6588539a588f72f6b7d9ea22c4f
oliverbaileysmith <smitholiverbailey@gmail.com>
2023-09-14 14:15:51 -0400

    Handle CORS

--- backend/index.js
+++ backend/index.js
@@ -1,5 +1,6 @@
 import express from "express";
 import mongoose from "mongoose";
+import cors from "cors";
 
 import { PORT, mongoDBURL } from "./config.js";
 import scoresRoute from "./routes/scoresRoutes.js";
@@ -8,6 +9,15 @@ const app = express();
 
 // Parse request body
 app.use(express.json());
+
+// Handle CORS
+app.use(
+	cors({
+		origin: "http://localhost:3000",
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		allowedHeaders: ["Content-Type"]
+	})
+);
 
 app.get("/", (req, res) => {
 	return res.status(234).send("Welcome to the leaderboard!");
