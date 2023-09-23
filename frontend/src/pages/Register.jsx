37d6b13ab9216dd252d2b521d739f40b5709ca62
oliverbaileysmith <smitholiverbailey@gmail.com>
2023-09-23 16:17:34 -0400

    Add placeholder register functionality

--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,7 +1,42 @@
-import React from "react";
+import React, {useContext} from "react";
+
+import LoginContext from "../LoginContext.jsx";
 
 const Register = () => {
-	return <h1>Register</h1>;
+	const loginContext = useContext(LoginContext);
+
+	const register = () => {
+		const reqBody = {
+			username: "gary",
+			password: "snail"
+		};
+		fetch("http://localhost:5555/api/users",
+		{
+			method: "POST",
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify(reqBody),
+			credentials: "include"
+		}).then(res => {
+			if (!res.ok)
+				throw new Error("Failed to register");
+			return res.json();
+		}).then(userData => {
+			loginContext.updateLogin({
+				isLoggedIn: true,
+				user: {
+					_id: userData._id,
+					username: userData.username
+				}
+			});
+		})
+		.catch(error => {
+			console.error(error.message);
+		});
+	};
+
+	return <button onClick={register}>register</button>
 };
 
 export default Register;
